e6675a832388b2db9d4acdd7e69e09a8c73b3c2c
Plutonium-239 <plutonium.239.811@gmail.com>
2025-10-06 19:45:25 +0530

    refactor the main function and allow multiple source paths - makes way for linux-line cp syntax by changing go-arg inputs - all args are sent to Args.Paths which is then parsed to find src/dest - dest is always Paths[-1] - breaks up gigantic main function into setup, parseArgs - robocopy passthrough arguments now need to be specified with `-[`

--- rbcp.go
+++ rbcp.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 	"runtime/pprof"
 	"slices"
@@ -25,25 +26,31 @@ const (
 	ProgramName = "rbcp"
 	Author      = "plutonium-239"
 )
+
 // : this will be injected by goreleaser at buildtime through ldflags
 var (
-	Version     = ""
-	Commit		= ""
-	BuildDate   = ""
+	Version   = ""
+	Commit    = ""
+	BuildDate = ""
 )
 
-var p *tea.Program
-var config Config
-var logger *log.Logger
+var (
+	p *tea.Program
+ 	config Config
+ 	logger *log.Logger
+ 	args Args
+ 	root string
+ 	dest string
+ 	files []string
+)
 
 type Args struct {
-	Src              string   `arg:"positional, required"`
-	Dest             string   `arg:"positional, required"`
+	Paths            []string `arg:"positional, required" placeholder:"SRC DEST"`
 	Mir              bool     `arg:"-m" help:"Convenience argument to specify /MIR to robocopy"`
 	List             bool     `arg:"-l" help:"Only list files that would be copied. Similar to a 'dry-run' "`
 	PreserveExitCode bool     `arg:"-p,--preserve-exitcode" help:"Always return the error code given by robocopy. By default, exit with code 0 on success and passthrough on copy failures."`
 	Insane           bool     `help:"Don't set sane defaults (currently sets #retries to 2 and timeout between them to 1 sec."`
-	OtherArgs        []string `arg:"positional" help:"All other arguments are passed directly to robocopy."`
+	OtherArgs        []string `arg:"-[,--passthrough" help:"All other arguments to be passed directly to robocopy."`
 	// !!! DISABLE IN PROD
 	Profile bool
 }
@@ -58,28 +65,13 @@ func (args Args) Description() string {
 func (Args) Version() string {
 	// impStyle will never be defined as GetConfig is never called in help/version text
 	impStyle = lipgloss.NewStyle().Foreground(lipgloss.Color(defaultConfig().Theme.ColorPrimary)).Bold(true)
-	return impStyle.Render(ProgramName + " version " + Version) + "\n" +
+	return impStyle.Render(ProgramName+" version "+Version) + "\n" +
 		"Commit: " + Commit + "\n" +
 		"Built: " + BuildDate
 }
 
-func (args Args) buildRobocopyArgs() []string {
-	out := []string{args.Src, args.Dest}
-	out = append(out, args.OtherArgs...)
-	if args.Mir {
-		out = append(out, "/MIR")
-	}
-	return out
-}
-
-
-func main() {
-	logger = log.New(os.Stderr)
-
-	// # Argument parsing and applying effects
-	var args Args
-	arg.MustParse(&args)
-
+// # Setup logging, config
+func setup() int {
 	lvl := "warn"
 	if envLoglvl := os.Getenv("LOGLEVEL"); envLoglvl != "" {
 		lvl = envLoglvl
@@ -103,7 +95,11 @@ func main() {
 		Background(lipgloss.Color(config.Theme.ColorError)).Foreground(lipgloss.Color("#fff")).
 		SetString("ERROR").Padding(0, 1).Bold(true)
 	logger.SetStyles(styles)
+	return initWidth
+}
 
+// # Parse arguments (break Paths into Src and Dest, perform expansions, build arguments for robocopy)
+func parseArgs() {
 	if args.Profile {
 		os.MkdirAll("prof/", os.ModeDir)
 		runtime.SetBlockProfileRate(1)
@@ -122,49 +118,106 @@ func main() {
 		defer pprof.StopCPUProfile()
 	}
 
-	// TODO: maybe check if user is passing a file as input/dest arg and wants to just copy one file
-	// - writing rbcp <path1> <path2> filename doesn't make much sense and doesnt allow cmdline helpers to suggest filenames
-	arrow := pathStyle.Italic(false).Render(" --> ")
-	if config.UseNerdFontArrow {
-		arrow = pathStyle.Italic(false).Render(" ─── ")
+	// stores the positions of files in args.Paths
+	files = make([]string, 0)
+
+	if len(args.Paths) < 2 {
+		logger.Fatal("No destination specified")
 	}
-	fmt.Println(lipgloss.PlaceHorizontal(initWidth, lipgloss.Center, pathStyle.Render(args.Src) + arrow + pathStyle.Render(args.Dest)))
-	rbarglist := args.buildRobocopyArgs()
-	log.Infof("Starting compact robocopy with arguments: %v", rbarglist)
-	logger.Infof("Starting compact robocopy with arguments: %v", rbarglist)
-	startTime := time.Now()
+	dest = args.Paths[len(args.Paths)-1]
+
+
+	// : multiple files
+	for _, srcf := range args.Paths[:len(args.Paths)-1] {
+		_, err := os.Stat(srcf)
+		if err == nil {
+			p, f := filepath.Split(srcf)
+			if p == "" {
+				logger.Debugf("Empty path, setting to ./")
+				p = "./"
+			}
+			if root == "" {
+				root = p
+			}
+			if root != p {
+				logger.Warnf("File passed not in root dir(%v): %v", root, srcf)
+			}
+			logger.Infof("Detected file %v and broke into %v and %v", srcf, p, f)
+			// args.Src[i] = p
+			// slices.Insert(args.OtherArgs, 0, f)
+			files[i] = f
+		} else {
+			logger.Errorf(errorStyle.Render("The file trying to be copied does not exist.\n%v"), err.Error())
+			os.Exit(1)
+		}
+	}
+
+}
+
+// # builds arguments for robocopy based on args. no side effects.
+func buildRobocopyArgs() []string {
+	out := []string{root, dest}
+	out = slices.Concat(out, files, args.OtherArgs)
+	// out = append(out, args.OtherArgs...)
+	if args.Mir {
+		out = append(out, "/MIR")
+	}
+	logger.Infof("Starting robocopy with arguments: %v", out)
 
 	if !args.List {
 		// Add our output formatting flags
 		notAllowed := []string{"/bytes", "/np", "/njh", "/njs", "/ndl", "/nfl", "/ns"}
-		slices.DeleteFunc(rbarglist, func(e string) bool {
+		slices.DeleteFunc(out, func(e string) bool {
 			return slices.Contains(notAllowed, strings.ToLower(e))
 		})
 		// let user log if wanted, but we need output to function so tee it
-		for _, e := range rbarglist {
+		for _, e := range out {
 			e = strings.ToLower(e)
 			if strings.HasPrefix(e, "/log") || strings.HasPrefix(e, "/unilog") {
-				rbarglist = append(rbarglist, "/tee")
+				out = append(out, "/tee")
 			}
 		}
-		rbarglist = append(rbarglist, "/NJH", "/NDL", "/BYTES")
+		out = append(out, "/NJH", "/NDL", "/BYTES")
 		if !args.Insane {
-			rbarglist = append(rbarglist, "/R:2", "/W:1")
+			out = append(out, "/R:2", "/W:1")
 		}
 	} else {
 		fmt.Println()
 	}
+	return out
+}
 
-	totalFiles, totalBytes, err := getTotalCounts(args)
+func main() {
+	logger = log.New(os.Stderr)
+
+	// : Argument parsing and applying effects
+	arg.MustParse(&args)
+
+	initWidth := setup()
+	startTime := time.Now()
+	parseArgs()
+
+	arrow := pathStyle.Italic(false).Render(" --> ")
+	if config.UseNerdFontArrow {
+		arrow = pathStyle.Italic(false).Render(" ─── ")
+	}
+	fmt.Println(lipgloss.PlaceHorizontal(initWidth, lipgloss.Center,
+		pathStyle.Render(root+"["+strings.Join(files, ",")+"]")+arrow+pathStyle.Render(args.Paths[len(args.Paths)-1])))
+
+	rbarglist := buildRobocopyArgs()
+
+	// : Dummy list-only run to get an overview of total
+	// if args.List is passed the program terminates inside this
+	totalFiles, totalBytes, err := getTotalCounts()
 	if err != nil {
 		logger.Fatalf("Error getting total counts: %v", err)
 	}
 	logger.Infof("Total to copy: %d files, %s\n", totalFiles, formatByteValue(totalBytes))
 
-	// # Init TUI and  start robocopy
+	// : Init TUI and  start robocopy
 	m := model{
-		progress:   progress.New(
-			progress.WithGradient(config.Theme.ColorProgress[0], config.Theme.ColorProgress[1]), 
+		progress: progress.New(
+			progress.WithGradient(config.Theme.ColorProgress[0], config.Theme.ColorProgress[1]),
 			// progress.WithSpringOptions(40, 1),
 		),
 		totalFiles: totalFiles,
@@ -209,7 +262,7 @@ func main() {
 	}
 
 	<-ended
-	// # Display summary
+	// : Display summary
 	logger.Infof("Robocopy took %v", robocopyEnd.Sub(robocopyStart))
 	logger.Infof("Waited for %v", time.Since(robocopyEnd))
 	displaySummary(stats)
@@ -247,7 +300,7 @@ func runRobocopy(args []string, forceQuit chan struct{}) (RobocopyStats, error) 
 	}
 
 	ended := make(chan struct{})
-	var parsingTime time.Duration 
+	var parsingTime time.Duration
 	go func() {
 		parsingStart := time.Now()
 		err = parseStreaming(stdout, &stats)
@@ -280,9 +333,9 @@ func runRobocopy(args []string, forceQuit chan struct{}) (RobocopyStats, error) 
 }
 
 // getTotalCounts runs robocopy in list-only mode to get total files and bytes
-func getTotalCounts(args Args) (int, int64, error) {
+func getTotalCounts() (int, int64, error) {
 	// Clone args and add /L to make it "list only" mode
-	rbargs := args.buildRobocopyArgs()
+	rbargs := buildRobocopyArgs()
 	listArgs := make([]string, len(rbargs))
 	copy(listArgs, rbargs)
 	if !args.List {
